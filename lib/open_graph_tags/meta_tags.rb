2bd8b4bc97e6758634064e68eeb9bfcead89c55b
Nuno Duarte <n.duarte@karma-consulting.pt>
2014-06-02 15:11:58 +0100

    Twitter Card Ready

--- lib/open_graph_tags/meta_tags.rb
+++ lib/open_graph_tags/meta_tags.rb
@@ -1,43 +1,37 @@
 # Controller extension
-module OpenGraphTags
+module OpenGraphTags::MetaTags
+  def self.included(base)
+    base.send(:before_filter, :set_meta_tags) if base.respond_to?(:before_filter)
+  end
 
-  module MetaTags
+  # before_filter to fetch meta tags from a cms file
+  def set_meta_tags
+    @meta_tags ||= Hash.new
 
-    def self.included(base)
-      base.send(:before_filter, :set_meta_tags) if base.respond_to?(:before_filter)
+    return unless @cms_page.respond_to?(:meta_information)
+
+    request_uri = request.env['PATH_INFO']
+
+    if @cms_page
+      @meta_tags = @cms_page.meta_information
+    elsif @cms_page = get_cms_page_for(request_uri)
+      @meta_tags = @cms_page.meta_information
+    elsif @cms_page = OpenGraphTags.config[:cms_page].try(:root)
+      @meta_tags = @cms_page.meta_information
+      @meta_tags[:url] = request_uri if @meta_tags
     end
+  end
 
-    # before_filter to fetch meta tags from a cms file
-    def set_meta_tags
-      @meta_tags ||= Hash.new
+  # method that allows to override predefined meta tags
+  def meta_tags(tags = {})
+    @meta_tags = (@meta_tags || {}).merge(tags)
+    return @meta_tags
+  end
 
-      return unless @cms_page.respond_to?(:meta_information)
-
-      request_uri = request.env['PATH_INFO']
-
-      if @cms_page
-        @meta_tags = @cms_page.meta_information
-      elsif @cms_page = get_cms_page_for(request_uri)
-        @meta_tags = @cms_page.meta_information
-      elsif @cms_page = OpenGraphTags.config[:cms_page].try(:root)
-        @meta_tags = @cms_page.meta_information
-        @meta_tags[:url] = request_uri if @meta_tags
-      end
-    end
-
-    # method that allows to override predefined meta tags
-    def meta_tags(tags = {})
-      @meta_tags = (@meta_tags || {}).merge(tags)
-      return @meta_tags
-    end
-
-    private
+  private
 
     def get_cms_page_for(request_uri)
       path = "/#{request_uri.split(OpenGraphTags.config[:root_uri])[1..-1].join("/")}"
       OpenGraphTags.config[:cms_page].where(:full_path => path).first
     end
-
-  end
-
 end
--- lib/open_graph_tags/view_helpers.rb
+++ lib/open_graph_tags/view_helpers.rb
@@ -1,48 +1,44 @@
 require 'uri'
 
-module OpenGraphTags
-	module ViewHelpers
+module OpenGraphTags::ViewHelpers
+	include OpenGraphTags::MetaTags
 
-		include OpenGraphTags::MetaTags
+	# partial rendering helper
+	def open_graph_meta_tags
+		set_meta_tags if meta_tags.keys.empty?
+		render :partial => "open_graph_tags/metatags", :locals => { :metatags => @meta_tags }
+	end
 
-		# partial rendering helper
-		def open_graph_meta_tags
-			set_meta_tags if meta_tags.keys.empty?
-			render :partial => "open_graph_tags/metatags", :locals => { :metatags => @meta_tags }
+	# tags helpers
+	def facebook_meta_tag(tag_name, content)
+		if OpenGraphTags.config[:providers].include?(:facebook)
+			tag(:meta, :content => content, :property => "og:#{tag_name}") unless content.blank?
 		end
+	end
 
-		# tags helpers
-		def facebook_meta_tag(tag_name, content)
-			if OpenGraphTags.config[:providers].include?(:facebook)
-				tag(:meta, :content => content, :property => "og:#{tag_name}") unless content.blank?
-			end
+	def twitter_meta_tag(tag_name, content)
+		if OpenGraphTags.config[:providers].include?(:twitter)
+			tag(:meta, :content => content, :name => "twitter:#{tag_name}") unless content.blank?
 		end
+	end
 
-		def twitter_meta_tag(tag_name, content)
-			if OpenGraphTags.config[:providers].include?(:twitter)
-				tag(:meta, :content => content, :name => "twitter:#{tag_name}") unless content.blank?
-			end
+	def google_meta_tag(tag_name, content)
+		if OpenGraphTags.config[:providers].include?(:google)
+			tag(:meta, :content => content, :itemprop => tag_name) unless content.blank?
 		end
+	end
 
-		def google_meta_tag(tag_name, content)
-			if OpenGraphTags.config[:providers].include?(:google)
-				tag(:meta, :content => content, :itemprop => tag_name) unless content.blank?
-			end
+	def different_providers
+		return (OpenGraphTags.config[:providers] - OpenGraphTags::DEFAULT_PROVIDERS)
+	end
+
+	def get_absolute_url(url, host)
+		uri = URI(url)
+		unless uri.absolute? || host.blank?
+			uri_host = URI(host)
+			uri.scheme = uri_host.scheme if uri_host.scheme
+			uri.host = uri_host.host
 		end
-
-		def different_providers
-			return (OpenGraphTags.config[:providers] - OpenGraphTags::DEFAULT_PROVIDERS)
-		end
-
-		def get_absolute_url(url, host)
-			uri = URI(url)
-			unless uri.absolute? || host.blank?
-				uri_host = URI(host)
-				uri.scheme = uri_host.scheme if uri_host.scheme
-				uri.host = uri_host.host
-			end
-			uri.to_s
-		end
-
+		uri.to_s
 	end
 end
